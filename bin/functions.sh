a4624bbb1076eb880797754a780c750050d95990
Colin Watson <colin.watson@canonical.com>
2005-09-05 17:30:47 +0000

    make publish-release prompt for confirmation if *.OVERSIZED exists

--- bin/functions.sh
+++ bin/functions.sh
@@ -4,3 +4,17 @@ mkemptydir () {
 	rm -rf "$1"
 	mkdir -p "$1"
 }
+
+confirm () {
+	printf ' [yN]'
+	read yesno
+	yesno="$(printf %s "$yesno" | tr A-Z a-z)"
+	case $yesno in
+		y|yes)
+			return 0
+			;;
+		*)
+			return 1
+			;;
+	esac
+}
