56ccb4fed4c884a6354e19483369e04e4396f4d1
Colin Watson <cjwatson@canonical.com>
2012-08-06 16:34:51 +0100

    sync-mirrors: convert to Python

--- lib/cdimage/mirror.py
+++ lib/cdimage/mirror.py
@@ -13,11 +13,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Logic to find which mirror to use."""
+"""Mirror handling."""
 
 __metaclass__ = type
 
 import os
+import subprocess
 
 
 class UnknownMirror(Exception):
@@ -47,3 +48,45 @@ def find_mirror(config, arch):
         return os.path.join(config.root, "ftp-ports")
     else:
         raise UnknownMirror("No mirror known for %s" % arch)
+
+
+class UnknownManifestFile(Exception):
+    pass
+
+
+def _trigger_mirror(key, user, host, background=False):
+    print("%s:", host)
+    command = [
+        "ssh", "-i", key,
+        "-o", "StrictHostKeyChecking no",
+        "-o", "BatchMode yes",
+        "%s@%s" % (user, host),
+        "sleep", "1",
+        ]
+    if background:
+        subprocess.Popen(command)
+    else:
+        subprocess.call(command)
+
+
+def trigger_mirrors(config):
+    # Check for non-existent files in .manifest.
+    simple_tree = os.path.join(config.root, "www", "simple")
+    with open(os.path.join(simple_tree, ".manifest")) as manifest:
+        for line in manifest:
+            name = line.rstrip("\n").split()[2]
+            if not os.path.exists(os.path.join(simple_tree, name.lstrip("/"))):
+                raise UnknownManifestFile(
+                    ".manifest has non-existent file %s" % name)
+
+    secret = os.path.join(config.root, "secret")
+    home_secret = os.path.expanduser("~/secret")
+    if os.path.isdir(home_secret):
+        secret = home_secret
+    key = os.path.join(secret, "auckland")
+
+    for host in config["TRIGGER_MIRRORS"].split():
+        _trigger_mirror(key, "archvsync", host)
+
+    for host in config["TRIGGER_MIRRORS_ASYNC"].split():
+        _trigger_mirror(key, "archvsync", host, background=True)
