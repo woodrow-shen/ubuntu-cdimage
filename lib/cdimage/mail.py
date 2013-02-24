b0dd9856c50ce022594db90f41d377c7df2d11de
Colin Watson <cjwatson@canonical.com>
2013-02-24 17:35:06 +0000

    daily-checks: move e-mail notification code to new cdimage.mail module

---
+++ lib/cdimage/mail.py
@@ -0,0 +1,86 @@
+# Copyright (C) 2013 Canonical Ltd.
+# Author: Colin Watson <cjwatson@ubuntu.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""E-mail notifications."""
+
+from __future__ import print_function
+
+__metaclass__ = type
+
+import os
+import subprocess
+import sys
+
+from cdimage.log import logger
+
+
+if sys.version >= "3":
+    import io
+    text_file_type = io.TextIOBase
+else:
+    text_file_type = file
+
+
+def _notify_addresses_path(config):
+    paths = [
+        os.path.join(config.root, "production", "notify-addresses"),
+        os.path.join(config.root, "etc", "notify-addresses"),
+    ]
+    for path in paths:
+        if os.path.exists(path):
+            return path
+    else:
+        return None
+
+
+def get_notify_addresses(config, project=None):
+    path = _notify_addresses_path(config)
+    if path is None:
+        return []
+    with open(path) as fp:
+        all_addresses = []
+        for line in fp:
+            this_project, addresses = line.split(None, 1)
+            if (this_project == "ALL" or
+                    (project is not None and this_project == project)):
+                all_addresses.extend(addresses.split())
+        return all_addresses
+
+
+def send_mail(subject, generator, recipients, body, dry_run=False):
+    if dry_run:
+        logger.info("Would send mail to: %s" % ", ".join(recipients))
+        logger.info("")
+        logger.info("Subject: %s" % subject)
+        logger.info("X-Generated-By: %s" % generator)
+        logger.info("")
+        if isinstance(body, text_file_type):
+            for line in body:
+                logger.info(line.rstrip("\n"))
+        else:
+            for line in body.splitlines():
+                logger.info(line)
+        logger.info("")
+    else:
+        command = [
+            "mail", "-s", subject, "-a", "X-Generated-By: %s" % generator]
+        command.extend(recipients)
+        if isinstance(body, text_file_type):
+            mailer = subprocess.Popen(command, stdin=body)
+        else:
+            mailer = subprocess.Popen(command, stdin=subprocess.PIPE)
+            print(body, end="", file=mailer.stdin)
+            mailer.stdin.close()
+        mailer.wait()
---
+++ lib/cdimage/tests/test_mail.py
@@ -0,0 +1,161 @@
+#! /usr/bin/python
+
+# Copyright (C) 2013 Canonical Ltd.
+# Author: Colin Watson <cjwatson@ubuntu.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Unit tests for cdimage.mail."""
+
+from __future__ import print_function
+
+__metaclass__ = type
+
+import os
+import subprocess
+
+import mock
+
+from cdimage.config import Config
+from cdimage.mail import (
+    _notify_addresses_path,
+    get_notify_addresses,
+    send_mail,
+)
+from cdimage.tests.helpers import TestCase
+
+
+class TestNotify(TestCase):
+    def setUp(self):
+        super(TestNotify, self).setUp()
+        self.config = Config(read=False)
+        self.config.root = self.use_temp_dir()
+
+    def test_notify_addresses_path(self):
+        self.assertIsNone(_notify_addresses_path(self.config))
+
+        path = os.path.join(self.temp_dir, "etc", "notify-addresses")
+        os.makedirs(os.path.dirname(path))
+        with open(path, "w"):
+            pass
+        self.assertEqual(path, _notify_addresses_path(self.config))
+
+        path = os.path.join(self.temp_dir, "production", "notify-addresses")
+        os.makedirs(os.path.dirname(path))
+        with open(path, "w"):
+            pass
+        self.assertEqual(path, _notify_addresses_path(self.config))
+
+    def test_get_notify_addresses_no_config(self):
+        self.assertEqual([], get_notify_addresses(self.config))
+
+    def test_get_notify_addresses_all_matches_any_project(self):
+        path = os.path.join(self.temp_dir, "production", "notify-addresses")
+        os.makedirs(os.path.dirname(path))
+        with open(path, "w") as f:
+            print("ALL\tfoo@example.org bar@example.org", file=f)
+        self.assertEqual(
+            ["foo@example.org", "bar@example.org"],
+            get_notify_addresses(self.config))
+        self.assertEqual(
+            ["foo@example.org", "bar@example.org"],
+            get_notify_addresses(self.config, "ubuntu"))
+        self.assertEqual(
+            ["foo@example.org", "bar@example.org"],
+            get_notify_addresses(self.config, "kubuntu"))
+
+    def test_get_notify_addresses_projects_match_exactly(self):
+        path = os.path.join(self.temp_dir, "production", "notify-addresses")
+        os.makedirs(os.path.dirname(path))
+        with open(path, "w") as f:
+            print("ubuntu\tubuntu@example.org", file=f)
+            print("kubuntu\tkubuntu@example.org", file=f)
+        self.assertEqual(
+            ["ubuntu@example.org"],
+            get_notify_addresses(self.config, "ubuntu"))
+        self.assertEqual(
+            ["kubuntu@example.org"],
+            get_notify_addresses(self.config, "kubuntu"))
+        self.assertEqual([], get_notify_addresses(self.config, "edubuntu"))
+
+    def test_send_mail_dry_run_from_file(self):
+        path = os.path.join(self.temp_dir, "body")
+        with open(path, "w") as body:
+            print("Body", file=body)
+            print("Text", file=body)
+        self.capture_logging()
+        with open(path) as body:
+            send_mail(
+                "Test subject", "test_notify", ["foo@example.org"], body,
+                dry_run=True)
+        self.assertLogEqual([
+            "Would send mail to: foo@example.org",
+            "",
+            "Subject: Test subject",
+            "X-Generated-By: test_notify",
+            "",
+            "Body",
+            "Text",
+            "",
+        ])
+
+    def test_send_mail_dry_run_from_string(self):
+        self.capture_logging()
+        send_mail(
+            "Test subject", "test_notify",
+            ["foo@example.org", "bar@example.org"], "Body\nText\n",
+            dry_run=True)
+        self.assertLogEqual([
+            "Would send mail to: foo@example.org, bar@example.org",
+            "",
+            "Subject: Test subject",
+            "X-Generated-By: test_notify",
+            "",
+            "Body",
+            "Text",
+            "",
+        ])
+
+    @mock.patch("subprocess.Popen")
+    def test_send_mail_from_file(self, mock_popen):
+        path = os.path.join(self.temp_dir, "body")
+        with open(path, "w") as body:
+            print("Body", file=body)
+            print("Text", file=body)
+        with open(path) as body:
+            send_mail(
+                "Test subject", "test_notify", ["foo@example.org"], body)
+            expected_command = [
+                "mail", "-s", "Test subject",
+                "-a", "X-Generated-By: test_notify",
+                "foo@example.org",
+            ]
+            self.assertEqual(
+                [mock.call(expected_command, stdin=body)],
+                mock_popen.call_args_list)
+
+    @mock.patch("subprocess.Popen")
+    def test_send_mail_from_string(self, mock_popen):
+        send_mail(
+            "Test subject", "test_notify",
+            ["foo@example.org", "bar@example.org"], "Body\nText\n")
+        expected_command = [
+            "mail", "-s", "Test subject", "-a", "X-Generated-By: test_notify",
+            "foo@example.org", "bar@example.org",
+        ]
+        self.assertEqual(
+            [mock.call(expected_command, stdin=subprocess.PIPE)],
+            mock_popen.call_args_list)
+        self.assertEqual(
+            [mock.call("Body\nText\n"), mock.call("")],
+            mock_popen.return_value.stdin.write.call_args_list)
