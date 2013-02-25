9fbe97163e6ff661092c13d1182e756a2d3f74c3
Colin Watson <cjwatson@canonical.com>
2013-02-25 08:31:28 +0000

    cdimage.tests.test_build_id: complete coverage

--- lib/cdimage/tests/test_build_id.py
+++ lib/cdimage/tests/test_build_id.py
@@ -23,6 +23,11 @@ try:
 except ImportError:
     from test.test_support import EnvironmentVarGuard
 
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
 from cdimage.build_id import next_build_id
 from cdimage.config import Config
 from cdimage.tests.helpers import TestCase
@@ -47,3 +52,48 @@ class TestNextBuildId(TestCase):
             self.assertEqual("20120806.1", next_build_id(config, "daily-live"))
             with open(stamp) as stamp_file:
                 self.assertEqual("20120806:2\n", stamp_file.read())
+
+    @mock.patch("time.strftime", return_value="20130225")
+    def test_defaults(self, *args):
+        with EnvironmentVarGuard() as env:
+            config = Config(read=False)
+            config.root = self.use_temp_dir()
+            config["PROJECT"] = "ubuntu"
+            config["DIST"] = "warty"
+            os.mkdir(os.path.join(self.temp_dir, "etc"))
+            stamp = os.path.join(
+                config.root, "etc", ".next-build-suffix-ubuntu-warty-daily")
+            self.assertFalse(os.path.exists(stamp))
+            self.assertEqual("20130225", next_build_id(config, ""))
+            with open(stamp) as stamp_file:
+                self.assertEqual("20130225:1\n", stamp_file.read())
+
+    @mock.patch("time.strftime", return_value="20130225")
+    def test_date_suffix(self, *args):
+        with EnvironmentVarGuard() as env:
+            config = Config(read=False)
+            config.root = self.use_temp_dir()
+            config["PROJECT"] = "ubuntu"
+            config["DIST"] = "warty"
+            config["DATE_SUFFIX"] = "5"
+            os.mkdir(os.path.join(self.temp_dir, "etc"))
+            stamp = os.path.join(
+                config.root, "etc", ".next-build-suffix-ubuntu-warty-daily")
+            self.assertFalse(os.path.exists(stamp))
+            self.assertEqual("20130225.5", next_build_id(config, "daily"))
+            with open(stamp) as stamp_file:
+                self.assertEqual("20130225:6\n", stamp_file.read())
+
+    def test_debug(self):
+        with EnvironmentVarGuard() as env:
+            config = Config(read=False)
+            config.root = self.use_temp_dir()
+            config["PROJECT"] = "ubuntu"
+            config["DIST"] = "warty"
+            config["DEBUG"] = "1"
+            os.mkdir(os.path.join(self.temp_dir, "etc"))
+            stamp = os.path.join(
+                config.root, "etc", ".next-build-suffix-ubuntu-warty-daily")
+            self.assertFalse(os.path.exists(stamp))
+            next_build_id(config, "daily")
+            self.assertFalse(os.path.exists(stamp))
