3dff33d634b7cb64ec00ac3e2979439a5f292c4a
Colin Watson <cjwatson@canonical.com>
2013-07-01 17:55:33 +0100

    Use pyflakes3 when testing under Python 3.

--- lib/cdimage/tests/test_static.py
+++ lib/cdimage/tests/test_static.py
@@ -22,7 +22,9 @@ from __future__ import print_function
 __metaclass__ = type
 
 import os
+import re
 import subprocess
+import sys
 
 from cdimage import osextras
 from cdimage.tests.helpers import TestCase
@@ -61,14 +63,39 @@ class TestStatic(TestCase):
         self.assertEqual(0, len(output))
 
     def test_pyflakes_clean(self):
-        if not osextras.find_on_path("pyflakes"):
+        if sys.version < "3":
+            pyflakes = "pyflakes"
+        else:
+            pyflakes = "pyflakes3"
+        if not osextras.find_on_path(pyflakes):
             return
         if "SKIP_SLOW_TESTS" in os.environ:
             return
+
+        # Exclude handling based on run-pyflakes.py from reviewboard,
+        # licensed under the MIT License.
+        cur_dir = os.path.dirname(__file__)
+        exclusions_path = os.path.join(cur_dir, "%s.exclude" % pyflakes)
+        exclusions = set()
+        if os.path.exists(exclusions_path):
+            with open(exclusions_path, "r") as f:
+                for line in f:
+                    if not line.startswith("#"):
+                        exclusions.add(line.rstrip())
+
+        error = False
         subp = subprocess.Popen(
-            ["pyflakes"] + self.all_paths(),
+            [pyflakes] + self.all_paths(),
             stdout=subprocess.PIPE, universal_newlines=True)
         output = subp.communicate()[0].splitlines()
         for line in output:
-            print(line)
-        self.assertEqual(0, len(output))
+            if line.startswith("#"):
+                continue
+            line = line.rstrip()
+            canon_line = re.sub(r":[0-9]+:", ":*:", line, 1)
+            canon_line = re.sub(r"line [0-9]+", "line *", canon_line)
+            if canon_line not in exclusions:
+                print(line)
+                error = True
+
+        self.assertFalse(error)
