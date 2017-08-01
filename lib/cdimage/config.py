718bd8227a2f07f06b3690168865d42690b9ae10
Adam Conrad <adconrad@0c3.net>
2017-08-01 02:57:34 -0600

    Update xenial to 16.04.3

--- lib/cdimage/config.py
+++ lib/cdimage/config.py
@@ -185,7 +185,7 @@ all_series.extend([
     Series("wily", "15.10", "Wily Werewolf"),
     Series(
         "xenial", "16.04", "Xenial Xerus",
-        pointversion="16.04.2",
+        pointversion="16.04.3",
         all_lts_projects=True),
     Series("yakkety", "16.10", "Yakkety Yak"),
     Series("zesty", "17.04", "Zesty Zapus"),
