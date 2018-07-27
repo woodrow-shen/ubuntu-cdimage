e53d82257de3d1a0c8bd450378181feb4befdc7e
Adam Conrad <adconrad@0c3.net>
2018-07-26 23:52:57 -0600

    Fix paths for publishing stable live-server images.

--- lib/cdimage/tree.py
+++ lib/cdimage/tree.py
@@ -3233,6 +3233,9 @@ class ReleasePublisher(Publisher):
             if source == "ubuntu-server/daily":
                 source = os.path.join(
                     "ubuntu-server", series.full_name, "daily")
+            elif source == "ubuntu-server/daily-live":
+                source = os.path.join(
+                    "ubuntu-server", series.full_name, "daily-live")
             elif source == "ubuntu-server/daily-preinstalled":
                 source = os.path.join(
                     "ubuntu-server", series.full_name, "daily-preinstalled")
