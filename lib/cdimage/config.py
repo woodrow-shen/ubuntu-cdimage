67e5fbdde19814d1a3b7060e946d45aaf00a8075
Steve Langasek <steve.langasek@canonical.com>
2018-11-29 15:20:15 -0800

    Update bionic to 18.04.1.0

--- lib/cdimage/config.py
+++ lib/cdimage/config.py
@@ -192,7 +192,7 @@ all_series.extend([
     Series("artful", "17.10", "Artful Aardvark"),
     Series(
         "bionic", "18.04", "Bionic Beaver",
-        pointversion="18.04.1",
+        pointversion="18.04.1.0",
         all_lts_projects=True),
     Series("cosmic", "18.10", "Cosmic Cuttlefish"),
     Series("disco", "19.04", "Disco Dingo"),
